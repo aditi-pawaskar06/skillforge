ea9379698fffb3c71b12398c10060ee91e239a06
Aditi Pawaskar <aditi.pawaskar2000@gmail.com>
2025-12-14 23:13:34 +0530

    feat: add basic skill input and interactive list

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,18 @@
-// placeholder for server.js
+// select DOM elements
+const skillInput = document.getElementById('skill-input');
+const addSkillBtn = document.getElementById('add-skill-btn');
+const skillsList = document.getElementById('skills-list');
+
+// handle button click
+addSkillBtn.addEventListener("click", ()=>{
+    const value = skillInput.value.trim();
+
+    if(value == "") return;
+    // create list item
+    const li = document.createElement("li");
+    li.textContent = value;
+    skillsList.appendChild(li);
+
+    // clear input field
+    skillInput.value = "";
+})
