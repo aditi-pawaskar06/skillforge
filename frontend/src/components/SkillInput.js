5b05b95179851efd68bc89a3fbf70833f6cefbcf
Aditi Pawaskar <aditi-pawaskar06>
2025-12-17 23:38:42 +0530

    feat: persist skills using localstorage

--- frontend/src/components/SkillInput.js
+++ frontend/src/components/SkillInput.js
@@ -1,15 +1,19 @@
 import { renderSkills } from "./SkillList.js";
 
-let skills = [];
+let skills = JSON.parse(localStorage.getItem("skills")) || [];
+
 export function setupSkillInput() {
   const skillInput = document.getElementById("skill-input");
   const addSkillBtn = document.getElementById("add-skill-btn");
+
+  renderSkills(skills);
 
   addSkillBtn.addEventListener("click", () => {
     const value = skillInput.value.trim();
     if (value == "") return;
 
     skills.push(value);
+    localStorage.setItem("skills", JSON.stringify(skills));
     renderSkills(skills);
 
     skillInput.value = "";
